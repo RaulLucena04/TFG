bfd61972dcd9d29dc3e814fc19e5c0bc8eb62733
RaulLucena04 <RaulLucena04@gmail.com>
2026-02-08 23:01:52 +0100

    Base de datos + reestructura

---
+++ nba-backend/src/main/java/com/tfg/nba_backend/model/Usuario.java
@@ -0,0 +1,22 @@
+package com.tfg.nba_backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private int puntos;
+
+    // getters y setters
+}
---
+++ nba-backend/src/test/java/com/tfg/nba_backend/NbaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.nba_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
